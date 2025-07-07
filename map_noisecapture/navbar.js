ac893ce544aece8bf9ca622e094b4df322d99359
nicolas-f <github@nettrader.fr>
2025-07-07 10:02:08 +0200

    data page is hosted on noise-planet.org and data files are still on the mirror site data.noise-planet.org (same as onomap-gs.noise-planet.org)

--- map_noisecapture/navbar.js
+++ map_noisecapture/navbar.js
@@ -44,7 +44,7 @@ document.write("                            <li> <a href=\"../noisemodelling_day
 document.write("                            <li> <a href=\"../noisemodelling_days_2021.html\" class=\"smoothScroll\">NoiseModelling days 2021</a></li>");
 document.write("                            </ul>");
 document.write("                            <li> <a href=\"../map.html\" class=\"smoothScroll\"><i class=\"fa fa-map\" aria-hidden=\"true\"></i> Map</a></li>");
-document.write("                            <li> <a href=\"http://data.noise-planet.org\/index.html\" class=\"smoothScroll\"><i class=\"fa fa-download\" aria-hidden=\"true\"></i> Data</a></li>");
+document.write("                            <li> <a href=\"../noisecapture_data.html\" class=\"smoothScroll\"><i class=\"fa fa-download\" aria-hidden=\"true\"></i> Data</a></li>");
 document.write("                            <li> <a href=\"../support.html\" class=\"smoothScroll\"><i class=\"fa fa-rocket\" aria-hidden=\"true\"></i> Support</a></li>");
 document.write("                            <li> <a href=\"../contact.html\" class=\"smoothScroll\"><i class=\"fa fa-envelope-o\" aria-hidden=\"true\"></i> Contact</a></li>");
 document.write("                         </ul>");
